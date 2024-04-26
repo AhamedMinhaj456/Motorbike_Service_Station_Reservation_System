d13210dafd3060599fcb3bd61a2b80875c1e8c43
AhamedMinhaj456 <139200872+AhamedMinhaj456@users.noreply.github.com>
2024-04-26 14:12:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,42 @@ import AdminProfileWindow from "./admin/views/AdminprofileWindow";
 import AccountSettingWindow from "./admin/views/AccountSettingWindow";
 import ChatSettingWindow from "./admin/views/ChatSettingWindow";
 import ShopViewDetailsWindow from "./admin/views/ShopViewDetailsWindow";
+
+
+const App = () =>{
+  return(
+   <Router>
+      <Routes>
+        <Route path="/" element={<AdminHomePage />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard  />} />
+        <Route path="/" element={<LeftSidebar />} />
+        <Route path="/fault-management" element={<FaultManagementWindow />} />
+        <Route path="/user-management" element={<UserManagementWindow />} />
+        <Route path="/shop-management" element={<ShopManagementWindow />} />
+        <Route path="/subscription-plans" element={<SubscriptionPlansManagementWindow />} />
+        <Route path="/profile" element={<AdminProfileWindow />} />
+        <Route path="/account-setting" element={<AccountSettingWindow />} />
+        <Route path="/chat-setting" element={<ChatSettingWindow />} />
+        <Route path="/shop-1" element={<ShopViewDetailsWindow />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './admin/views/AdminDashboard';
+import LeftSidebar from "./admin/common/LeftSidebar";
+import FaultManagementWindow from "./admin/views/FaultManagementWindow";
+import UserManagementWindow from "./admin/views/UserManagementWindow";
+import ShopManagementWindow from "./admin/views/ShopManagementWindow";
+import SubscriptionPlansManagementWindow from "./admin/views/SubscriptionPlansManagamentWindow";
+import AdminHomePage from "./admin/views/AdminHomePage";
+import AdminProfileWindow from "./admin/views/AdminprofileWindow";
+import AccountSettingWindow from "./admin/views/AccountSettingWindow";
+import ChatSettingWindow from "./admin/views/ChatSettingWindow";
 import AddFault from "./admin/views/AddFault";
 
 
@@ -30,7 +66,6 @@ const App = () =>{
         <Route path="/profile" element={<AdminProfileWindow />} />
         <Route path="/account-setting" element={<AccountSettingWindow />} />
         <Route path="/chat-setting" element={<ChatSettingWindow />} />
-        <Route path="/shop-1" element={<ShopViewDetailsWindow />} />
       </Routes>
     </Router>
 
