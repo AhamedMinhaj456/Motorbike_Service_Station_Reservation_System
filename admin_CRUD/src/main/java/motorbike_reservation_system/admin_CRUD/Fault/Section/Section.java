e8720841b8eb28482d0585d0b86cd233ca2000f4
AhamedMinhaj456 <139200872+AhamedMinhaj456@users.noreply.github.com>
2024-05-01 23:17:09 +0530

    company entity edited

--- admin_CRUD/src/main/java/motorbike_reservation_system/admin_CRUD/Fault/Section/Section.java
+++ admin_CRUD/src/main/java/motorbike_reservation_system/admin_CRUD/Fault/Section/Section.java
@@ -71,3 +71,78 @@ public class Section {
         return sb.toString();
     }
 }
+
+//package motorbike_reservation_system.admin_CRUD.Fault.Section;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.PrePersist;
+//import jakarta.persistence.Table;
+//
+//import java.util.Random;
+//
+//@Entity
+//@Table(name="section")
+//public class Section {
+//
+//    @Id
+//    private String sectionId;
+//
+//    private String sectionName;
+//
+//    public Section() {
+//    }
+//
+//    public Section(String sectionId, String sectionName) {
+//        this.sectionId = sectionId;
+//        this.sectionName = sectionName;
+//    }
+//
+//    public Section(String sectionName) {
+//        this.sectionName = sectionName;
+//    }
+//
+//    public String getSectionId() {
+//        return sectionId;
+//    }
+//
+//    public void setSectionId(String sectionId) {
+//        this.sectionId = sectionId;
+//    }
+//
+//    public String getSectionName() {
+//        return sectionName;
+//    }
+//
+//    public void setSectionName(String sectionName) {
+//        this.sectionName = sectionName;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "section{" + "sectionId=" + sectionId + ", sectionName=" + sectionName + '}';
+//    }
+//
+//    /////// Custom logic to generate the next available ID //////////////////////////////////////
+//
+//    @PrePersist
+//    public void generateId() {
+//        if (sectionId == null) {
+//            // Generate a random 6-digit number
+//            String randomDigits = generateRandomDigits(6);
+//
+//            // Assign the ID as "FM" followed by the random digits
+//            this.sectionId = "Sec" + randomDigits;
+//        }
+//    }
+//
+//    private String generateRandomDigits(int length) {
+//        Random random = new Random();
+//        StringBuilder sb = new StringBuilder(length);
+//        for (int i = 0; i < length; i++) {
+//            sb.append(random.nextInt(10));
+//        }
+//        return sb.toString();
+//    }
+//}
+
